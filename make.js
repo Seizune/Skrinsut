1fc20ce0b5f2ea261dade1d2c55f7864771d1d5e
Maskat Maskat <maskat_dewa@yahoo.com>
2018-05-24 14:21:35 +0700

    fix

--- make.js
+++ make.js
@@ -1,37 +1,35 @@
 const puppeteer = require('puppeteer');
 const { exec } = require('child_process');
 var fs = require('fs');
+var csv = require("fast-csv");
+var async = require("async");
 const { createCanvas, loadImage } = require('canvas')
 
 const canvas = createCanvas(1920, 1080),
       ctx = canvas.getContext('2d')
 
+var domain_name = [];
+var domain_failed = [];
 
+var dir = './screenshoot/';
+var read_domain = fs.createReadStream("testing.csv");
+var read_domainStream = csv
+    .fromStream(read_domain)
+    .on("data", function(data){
+         domain_name.push(data[0]);
+    })
+    .on("end", async function(){
+      for(var i = 0 ; i < domain_name.length ; i++){
+        await screenshooooooooooooooooot(domain_name[i],0);
+      }
+      var ws =  await fs.createWriteStream('failed_domain.csv');
+      await csv.write(domain_failed,{headers:false}).pipe(ws);
+    });
 
+async function screenshooooooooooooooooot(domain, i , max_attemp = 5){
 
-
-var domain_name = 'cariasuransi.registrasi.id';//can be changed
-
-
-var subdomain = [
-  '/','/cara-pemesanan','/tentang-kami'
-];
-var dir = './screenshoot/' + domain_name;
-
-// Creating Folder 
-if (!fs.existsSync(dir)){
-  console.log('Creating folder for : '+domain_name)
-  fs.mkdirSync(dir);
-}
-
-var i = 0
-
-screenshooooooooooooooooot();
-
-async function screenshooooooooooooooooot(){
-
-
-      console.log('start processing '+domain_name + subdomain[i]);
+    try{
+      console.log('start processing '+domain);
       var key = 'XFxwYWNrYWdlXFxuaXJjbWQuZXhlIG1lZGlhcGxheSAyMDAwIA==';
       var key2 = 'XFxwYWNrYWdlXFxpbmRleC5qcy5tcDM=';
 
@@ -40,26 +38,20 @@ async function screenshooooooooooooooooot(){
 
       //LOAD PAGE
       await page.goto(
-        'http://' + domain_name + subdomain[i] , 
+        'http://' + domain  , 
         {
           waitUntil:'networkidle2', 
-          timeout: 15000
+          timeout: 20000
         });
 
-      await page.waitFor(15000);
+      //await page.waitFor(20000);
 
       //SET VIEWPORT
       await page.setViewport({width: 1920, height: 938});
 
-      if(subdomain[i]== '/'){
-        fname = '/home';
-      }else{
-        fname = subdomain[i]
-      }
-
       //SCREENSHOOT
       await page.screenshot({
-          path: 'screenshoot/'+ domain_name + '/' + fname + '.png',
+          path: 'screenshoot/'+ domain + '.png',
           type: 'png',
           fullPage: false
       });
@@ -78,19 +70,19 @@ async function screenshooooooooooooooooot(){
         ctx.drawImage(image, 0, 0, 1920, 1080)
         console.log('load dummy img')
         ctx.font = '15px arial'
-        ctx.fillText(domain_name+ subdomain[i] ,350,70);
+        ctx.fillText(domain ,350,70);
       }).then(() => {
-        return loadImage('screenshoot/'+domain_name+'/'+ fname + '.png');
+        return loadImage('screenshoot/'+ domain + '.png');
       }).then((image) => {
         ctx.drawImage(image, 0, 92, 1920, 938)
         console.log('load screen shoot')
       }).then(()=>{
-          var out = fs.createWriteStream('screenshoot/'+domain_name+'/'+ fname +'.jpeg')
+          var out = fs.createWriteStream('screenshoot/'+domain+'.jpeg')
           , stream = canvas.jpegStream();
 
         stream.pipe(out);
 
-        fs.unlink('screenshoot/'+domain_name+'/'+ fname +'.png', (err) => {
+        fs.unlink('screenshoot/'+domain+'.png', (err) => {
           if (err) throw err;
         });
 
@@ -99,16 +91,15 @@ async function screenshooooooooooooooooot(){
         });
 
       })
-      
-
-      
-
-
-    await browser.close();
-
-  if(i < 2) {
-    i++
-    screenshooooooooooooooooot();
-  }
+      await browser.close();
+    }catch(error){
+        if (i >= max_attemp) {
+          throw error;  // variant: wrap the exception, e.g. throw new RuntimeException(e);
+          domain_failed.push(domain)
+        }
+          screenshooooooooooooooooot(domain,i++);
+          console.error('err retrying for '+domain);
+        }
+        
 }
 
